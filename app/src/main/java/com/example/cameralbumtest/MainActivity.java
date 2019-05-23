5d078e48b9a9f738bc4fe4bd1e5d7a4071ce9325
xiaoming <961775890@qq.com>
2019-05-23 23:01:13 +0800

    5.23 8.3 paizhao

--- app/src/main/java/com/example/cameralbumtest/MainActivity.java
+++ app/src/main/java/com/example/cameralbumtest/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.cameralbumtest;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.ContentUris;
 import android.content.Intent;
 import android.content.UriMatcher;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +25,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -27,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView picture;
 
+    private static final int CHOOSE_PHOTO = 2;
+
     private Uri imageUri;
 
     @Override
@@ -34,7 +46,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button takephoto = (Button)findViewById(R.id.take_photo);
+        Button chooseFromAlbum = (Button)findViewById(R.id.choose_from_album);
         picture = (ImageView)findViewById(R.id.picture);
+
+        chooseFromAlbum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+                }else {
+                    openAlbum();
+                }
+            }
+        });
+
         takephoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 if (Build.VERSION.SDK_INT >= 24){
-                    imageUri = FileProvider.getUriForFile(MainActivity.this,"com.example.cameraalbumtest.fileprovider",outputImage);
+                    imageUri = FileProvider.getUriForFile(MainActivity.this,"com.example.cameralbumtest.fileprovider",outputImage);
                 }else {
                     Log.d("123123","android sdk 24");
                     imageUri = Uri.fromFile(outputImage);
@@ -61,6 +86,26 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void openAlbum(){
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent,CHOOSE_PHOTO);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    openAlbum();
+                }else {
+                    Toast.makeText(this,"you denide the permission",Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         switch (requestCode){
@@ -69,16 +114,79 @@ public class MainActivity extends AppCompatActivity {
                     try{
                         Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
                         picture.setImageBitmap(bitmap);
-                        Log.d("123123","result_ok");
+//                        Log.d("123123","result_ok");
                     }catch (FileNotFoundException e){
-                        Log.d("123123","catch");
+//                        Log.d("123123","catch");
                         e.printStackTrace();
                     }
                 }
                 break;
+
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK){
+                    if (Build.VERSION.SDK_INT >= 19){
+                        handleImageOnKitkat(data);
+                    }else {
+                        handleImageBeforeKitkat(data);
+                    }
+                }
+                break;
             default:
-                Log.d("123123","defaule");
+//                Log.d("123123","defaule");
                 break;
         }
     }
+
+    @TargetApi(19)
+    private void handleImageOnKitkat(Intent data){
+        String imagePath = null;
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(this,uri)){
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())){
+                String id = docId.split(":")[1];
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+            }else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())){
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public/public_downloads"),Long.valueOf(docId));
+                imagePath = getImagePath(contentUri,null);
+            }else if ("content".equalsIgnoreCase(uri.getScheme())){
+                imagePath = getImagePath(uri,null);
+            }else if ("file".equalsIgnoreCase(uri.getScheme())){
+                imagePath = uri.getPath();
+            }
+        }
+        displayImage(imagePath);
+    }
+
+    private void handleImageBeforeKitkat(Intent data){
+        Uri uri = data.getData();
+        String imagepath = getImagePath(uri,null);
+        displayImage(imagepath);
+    }
+
+    private String getImagePath(Uri uri,String selection){
+        String path = null;
+        Cursor cursor = getContentResolver().query(uri,null,selection,null,null);
+        if (cursor != null){
+            if (cursor.moveToFirst()){
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+
+    private void displayImage(String imagepath){
+        if (imagepath != null){
+            Bitmap bitmap = BitmapFactory.decodeFile(imagepath);
+            picture.setImageBitmap(bitmap);
+        }else {
+            Toast.makeText(this,"faild to get image",Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+
 }
